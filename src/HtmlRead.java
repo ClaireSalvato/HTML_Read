9ca74f479997926b008dba22ccbb6ba0fc7379e1
Claire <148796047+ClaireSalvato@users.noreply.github.com>
2024-11-04 17:32:06 -0500

    Search for wiki in wikipedia link

---
+++ src/HtmlRead.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+    public class HtmlRead {
+
+        public static void main(String[] args) {
+            HtmlRead html = new HtmlRead();
+        }
+
+        public HtmlRead() {
+
+            try {
+                System.out.println();
+                System.out.print("hello \n");
+                URL url = new URL("https://en.wikipedia.org/wiki/Portugal");
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(url.openStream())
+                );
+                String line;
+                while ( (line = reader.readLine()) != null ) {
+                    if (line.contains("/wiki/"))
+                        System.out.println(line);
+                }
+                reader.close();
+            } catch(Exception ex) {
+                System.out.println(ex);
+            }
+
+        }
+
+    }
