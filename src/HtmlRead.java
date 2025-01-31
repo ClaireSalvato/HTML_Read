fbaf172fc2c040e13415a9f484dcffd435555184
Claire <148796047+ClaireSalvato@users.noreply.github.com>
2025-01-31 08:38:17 -0500

    Search for wiki in wikipedia link

--- src/HtmlRead.java
+++ src/HtmlRead.java
@@ -2,31 +2,42 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 
-    public class HtmlRead {
+public class HtmlRead {
 
-        public static void main(String[] args) {
-            HtmlRead html = new HtmlRead();
-        }
+    public static void main(String[] args) {
+        HtmlRead html = new HtmlRead();
+    }
 
-        public HtmlRead() {
+    public HtmlRead() {
 
-            try {
-                System.out.println();
-                System.out.print("hello \n");
-                URL url = new URL("https://en.wikipedia.org/wiki/Portugal");
-                BufferedReader reader = new BufferedReader(
-                        new InputStreamReader(url.openStream())
-                );
-                String line;
-                while ( (line = reader.readLine()) != null ) {
-                    if (line.contains("/wiki/"))
-                        System.out.println(line);
+        try {
+            System.out.println();
+            System.out.print("hello \n");
+            URL url = new URL("https://en.wikipedia.org/wiki/Portugal");
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(url.openStream())
+            );
+            String line;
+            int c = 1;
+            while ( (line = reader.readLine()) != null ) {
+
+                if (line.contains("/wiki/")){
+                    int start = line.indexOf("/wiki/")-10;
+                    while() {
+                        int end = start + 26;
+                        String miniline = line.substring(start, end);
+                        System.out.println(c + "." + miniline);
+                        c++;
+                        start = line.indexOf("/wiki/", start + 11);
+                    }
                 }
-                reader.close();
-            } catch(Exception ex) {
-                System.out.println(ex);
+                System.out.println(line);
             }
-
+            reader.close();
+        } catch(Exception ex) {
+            System.out.println(ex);
         }
 
     }
+
+}
